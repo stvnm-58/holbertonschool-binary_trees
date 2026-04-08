8b5dc152634d0a0cac90ffe5c46c37c38b789682
steven <12992@holbertonstudents.com>
2026-04-08 12:46:18 +0200

    correction

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,7 +13,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 
 	if (tree->left != NULL || tree->right != NULL)
-		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right) + 1);
+		return (nodes(tree->left) + nodes(tree->right) + 1);
 
 
 	else
