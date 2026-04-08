4a23538c6be40e435b1ff8dc5a3d98bb33665d1a
steven <12992@holbertonstudents.com>
2026-04-08 11:15:33 +0200

    correction

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,5 +1,5 @@
 #include "binary_trees.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * binary_tree_insert_left - Insère un nœud comme enfant gauche d'un autre nœud.
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	new_node = malloc(sizeof(binary_tree_t));
 		if (new_node == NULL)
-		return(NULL);
+		return (NULL);
 
 	new_node->parent = parent;
 	new_node->n = value;
@@ -40,6 +40,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	
 
 
-return(new_node);
+return (new_node);
 
 }
