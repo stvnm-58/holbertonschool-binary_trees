305e8fcff2cd61005292a954c5d99a684a763db5
steven <12992@holbertonstudents.com>
2026-04-08 12:06:25 +0200

    correction

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,44 +2,35 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_insert_left - Insère un nœud comme enfant gauche d'un autre nœud.
- * @parent: Pointeur vers le nœud où insérer l'enfant gauche.
+ * binary_tree_insert_left - Insère un nœud comme enfant gauche.
+ * @parent: Pointeur vers le nœud parent.
  * @value: Valeur à stocker dans le nouveau nœud.
  *
- * Return: Un pointeur vers le nœud créé, ou NULL en cas d'échec
- * ou si le parent est NULL.
+ * Return: Pointeur vers le nouveau nœud, ou NULL en cas d'échec.
  */
-
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+    binary_tree_t *new_node;
 
-	if (parent == NULL)
-	return (NULL);
+    if (parent == NULL)
+        return (NULL);
 
+    new_node = malloc(sizeof(binary_tree_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	new_node = malloc(sizeof(binary_tree_t));
-		if (new_node == NULL)
-		return (NULL);
+    new_node->n = value;
+    new_node->parent = parent;
+    new_node->right = NULL;
 
-	new_node->parent = parent;
-	new_node->n = value;
+    new_node->left = parent->left;
 
+    if (new_node->left != NULL)
+    {
+        new_node->left->parent = new_node;
+    }
 
-	if (parent->left == NULL)
-	parent->left = new_node;
+    parent->left = new_node;
 
-	if (parent->left != NULL)
-	{
-		new_node->left = parent->left;
-		new_node->left->parent = new_node;
-	}
-
-	parent->left = new_node;
-
-	
-
-
-return (new_node);
-
+    return (new_node);
 }
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,43 +1,36 @@
 #include "binary_trees.h"
 #include <stdlib.h>
+
 /**
- * binary_tree_insert_right - Insère un nœud comme enfant droit d'un autre nœud.
- * @parent: Pointeur vers le nœud où insérer l'enfant droit.
+ * binary_tree_insert_right - Insère un nœud comme enfant droit.
+ * @parent: Pointeur vers le nœud parent.
  * @value: Valeur à stocker dans le nouveau nœud.
  *
- * Return: Un pointeur vers le nœud créé, ou NULL en cas d'échec
- * ou si le parent est NULL.
+ * Return: Pointeur vers le nouveau nœud, ou NULL en cas d'échec.
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+    binary_tree_t *new_node;
 
-	if (parent == NULL)
-	return (NULL);
+    if (parent == NULL)
+        return (NULL);
 
+    new_node = malloc(sizeof(binary_tree_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	new_node = malloc(sizeof(binary_tree_t));
-		if (new_node == NULL)
-		return(NULL);
+    new_node->n = value;
+    new_node->parent = parent;
+    new_node->left = NULL;
+    
+    new_node->right = parent->right;
 
-	new_node->parent = parent;
-	new_node->n = value;
+    if (new_node->right != NULL)
+    {
+        new_node->right->parent = new_node;
+    }
 
+    parent->right = new_node;
 
-	if (parent->right == NULL)
-	parent->right = new_node;
-
-	if (parent->right != NULL)
-	{
-		new_node->right = parent->left;
-		new_node->right->parent = new_node;
-	}
-
-	parent->right = new_node;
-
-	
-
-
-return(new_node);
-
+    return (new_node);
 }
